1db95f2aa917253dcb10bbe1ebdea08adb8dac31
Filipe Santiago <filipe@upplify.com>
2020-08-23 14:41:28 -0300

    add inputs boxes

--- src/app/components/interpolation-result/interpolation-result.component.ts
+++ src/app/components/interpolation-result/interpolation-result.component.ts
@@ -22,6 +22,12 @@ export class InterpolationResultComponent {
     public dc: Point;
 
     constructor(private readonly coordinatesService: CoordinatesService) {
+        this.world = this.getInitialViewPort();
+        this.device = this.getInitialViewPort();
+        this.point = this.getInitialPoint();
+        this.ndc = this.getInitialPoint();
+        this.dc = this.getInitialPoint();
+
         this.subscriptions.sink = this.coordinatesService.onTransformation$.subscribe((result) => {
             const { world, device, point, ndc, dc } = result;
 
@@ -31,5 +37,13 @@ export class InterpolationResultComponent {
             this.ndc = ndc;
             this.dc = dc;
         });
+    }
+
+    private getInitialViewPort(): ViewPort {
+        return { x: { min: 0, max: 0 }, y: { min: 0, max: 0 } };
+    }
+
+    private getInitialPoint(): Point {
+        return { x: 0, y: 0 };
     }
 }
