f2109dca039fab85a9b04427aed13562d7e28b1f
Aghata Sophia <aghatasophia@gmail.com>
2020-08-30 20:49:32 -0300

    Updates Dda Result Component

--- src/app/components/line-dda-result/line-dda-result.component.ts
+++ src/app/components/line-dda-result/line-dda-result.component.ts
@@ -1,19 +1,30 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, Output, EventEmitter } from '@angular/core';
 import { CoordinatesService } from '../../services/coordinates.service';
 import { Point, ViewPort, NormalizedRange } from '../../types/coordinates';
-import { DdaMetadata } from '../../types/lines';
+import { DdaMetadata, DdaFixValue } from '../../types/lines';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-line-dda-result',
     templateUrl: './line-dda-result.component.html',
     styleUrls: ['./line-dda-result.component.scss'],
 })
+
 export class LineDdaResultComponent {
     @Input()
     public metadata: DdaMetadata;
 
     @Input()
     public point: Point;
+
+    @Output()
+    public onDrawLine: EventEmitter<DdaFixValue>;
+
+    constructor(private readonly fb: FormBuilder, private readonly coordinateService: CoordinatesService) {
+        this.onDrawLine = new EventEmitter;
+    }
+
+    public ddaForm: FormGroup;
 
     private viewPort: ViewPort = {
         x: {
@@ -26,7 +37,19 @@ export class LineDdaResultComponent {
         },
     };
 
-    constructor(private readonly coordinateService: CoordinatesService) {}
+    public ngOnInit(): void {
+        this.initForm();
+
+        if (!this.metadata) {
+            this.metadata = {
+                dx: 0,
+                dy: 0,
+                steps: 0,
+                xIncrement: 0,
+                yIncrement: 0
+            }
+        }
+    }
 
     public ngOnChanges(changes: SimpleChanges): void {
         this.transformPoint(changes.point.currentValue);
@@ -41,5 +64,19 @@ export class LineDdaResultComponent {
                 NormalizedRange.center,
             );
         }
+    }
+
+    public onFormSubmit(): void {
+        const value = this.ddaForm.value as DdaFixValue;
+        this.onDrawLine.emit(value);
+    }
+
+    private initForm(): void {
+        this.ddaForm = this.fb.group({
+            startPointX: [0, Validators.required],
+            startPointY: [0, Validators.required],
+            endPointX: [0, Validators.required],
+            endPointY: [0, Validators.required],
+        });
     }
 }
